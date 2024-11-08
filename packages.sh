aec029205802014207fc28ee19fcfc2e164adcb8
njg4ne <njg4ne@virginia.edu>
2024-11-08 15:52:57 -0500

    modifying packages

--- packages.sh
+++ packages.sh
@@ -1,2 +1,2 @@
-install2.r httpgd languageserver ggplot2 lme4 lmerTest coin effectsize dplyr stringr GGally psych lattice ggpubr gplots MASS ggfortify ggResidpanel pacman datasets devtools here lindia boot data.table plyr scales grid forecast tidyverse lubridate mtsdi car tseries MTS
+install2.r MVN httpgd languageserver ggplot2 lme4 lmerTest coin effectsize dplyr stringr GGally psych lattice ggpubr gplots MASS ggfortify ggResidpanel pacman datasets devtools here lindia boot data.table plyr scales grid forecast tidyverse lubridate mtsdi car tseries MTS
 
