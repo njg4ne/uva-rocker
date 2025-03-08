66c7f281a799e6fd6ee7f2a981902bf0dc343a37
njg4ne <njg4ne@virginia.edu>
2025-03-08 08:08:32 -0500

    need to be root to install

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
-id=$(docker create rocker/tidyverse)
-mkdir -p R
-docker cp $id:/usr/local/lib/R/library R/
-docker cp $id:/usr/local/lib/R/site-library R/
-docker rm -v $id
+#! /bin/sh
+read -p "Do you want to start fresh, deleting all your installed R packages? (y/n): " choice
+if [[ "$choice" == "y" ]]; then
+    rm -rf R
+    id=$(docker create rocker/tidyverse)
+    mkdir -p R
+    docker cp $id:/usr/local/lib/R/library R/
+    docker cp $id:/usr/local/lib/R/site-library R/
+    docker rm -v $id
+fi
+
